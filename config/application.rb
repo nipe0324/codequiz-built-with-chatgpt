40bcd06ca65026d0593bf2a3b8c234ca5cb9b987
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-05-05 20:45:41 +0900

    rails generate clearance:views

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,10 @@ module CodequizBuiltWithChatgpt
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Asia/Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
 
     # Don't generate system test files.
     config.generators.system_tests = nil
