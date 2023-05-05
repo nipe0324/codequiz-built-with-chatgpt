b29689621995be326450ffdc5b226c62943590c8
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-05-05 23:29:49 +0900

    set html title tag on each pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def html_title(page_title = '')
+    base_title = "Code Quiz"
+    if page_title.nil?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
