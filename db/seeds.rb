1a756bcf14aaa04b400689b11faafe1d44d39b4f
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-05-06 13:26:44 +0900

    Add Category model with categories table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+categories = [
+  { id: 1, name: 'Ruby' },
+  { id: 2, name: 'Go' },
+  { id: 3, name: 'Rust' }
+]
+
+categories.each do |category|
+  Category.find_or_create_by!(id: category[:id]) do |c|
+    c.name = category[:name]
+  end
+end
